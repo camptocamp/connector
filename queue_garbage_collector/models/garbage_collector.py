42ff096d6232a1a9e4cc0ce68d55735564910058
mpanarin <m.panarin@mobilunity.com>
2019-02-28 13:29:00 +0200

    make it a simple method and clean up

--- queue_garbage_collector/models/garbage_collector.py
+++ queue_garbage_collector/models/garbage_collector.py
@@ -5,27 +5,7 @@
 from datetime import timedelta
 
 from openerp import api, fields, models
-from openerp.addons.connector.queue.job import job
 from openerp.addons.connector.session import ConnectorSession
-
-
-@job
-def cron_checkup(session, model_name, in_queue_delta=5, started_delta=180):
-    """Fix jobs that are in a bad states
-    :param in_queue_delta: lookup time in minutes for jobs
-                            that are in enqueued state
-
-    :param started_delta: lookup time in minutes for jobs
-                            that are in enqueued state
-    """
-    obj = session.env[model_name]
-    obj.treat(
-        obj.get_jobs_to_requeue(
-            in_queue_delta=in_queue_delta,
-            started_delta=started_delta
-        )
-    )
-    return True
 
 
 class ConnectorQueueGarbageCollector(models.TransientModel):
@@ -33,34 +13,40 @@ class ConnectorQueueGarbageCollector(models.TransientModel):
     _name = "connector.queue.garbage.collector"
 
     @api.model
-    def set_cleanup_job(self):
-        session = ConnectorSession.from_env(self.env)
-        cron_checkup.delay(
-            session,
-            self._name,
+    def requeue_stuck_jobs(self, in_queue_delta=5, started_delta=180):
+        """Fix jobs that are in a bad states
+        :param in_queue_delta: lookup time in minutes for jobs
+                                that are in enqueued state
+
+        :param started_delta: lookup time in minutes for jobs
+                                that are in enqueued state
+        """
+        self.treat(
+            self.get_stuck_jobs_to_requeue(
+                in_queue_delta=in_queue_delta,
+                started_delta=started_delta
+            )
         )
+        jobs = self.get_stuck_jobs_to_requeue(
+            in_queue_delta=in_queue_delta,
+            started_delta=started_delta
+        )
+        for job in jobs:
+            job.requeue()
+        return True
 
     @api.multi
-    def get_jobs_to_requeue(self, in_queue_delta, started_delta):
-        job_object = self.env['queue.job']
+    def get_stuck_jobs_to_requeue(self, in_queue_delta, started_delta):
+        job_model = self.env['queue.job']
         now = fields.datetime.now()
         queue_dl = now - timedelta(minutes=in_queue_delta)
         started_dl = now - timedelta(minutes=started_delta)
-        sick_jobs = job_object.search(
+        stuck_jobs = job_model.search(
             [('date_enqueued', '<=', fields.Datetime.to_string(queue_dl)),
              ('state', '=', 'enqueued')]
         )
-        sick_jobs += job_object.search(
+        stuck_jobs += job_model.search(
             [('date_started', '<=', fields.Datetime.to_string(started_dl)),
              ('state', '=', 'started')]
         )
-        return sick_jobs
-
-    @api.multi
-    def treat(self, jobs):
-        self.requeue(jobs)
-
-    @api.multi
-    def requeue(self, jobs):
-        for job_ in jobs:
-            job_.requeue()
+        return stuck_jobs
