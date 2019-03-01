7fef560fb6828262c663660270fbc337ea95c77b
mpanarin <m.panarin@mobilunity.com>
2019-03-01 10:11:37 +0200

    fixup! move to the connector module

--- connector/queue/model.py
+++ connector/queue/model.py
@@ -222,28 +222,29 @@ class QueueJob(models.Model):
         :param started_delta: lookup time in minutes for jobs
                                 that are in enqueued state
         """
-        jobs = self.get_stuck_jobs_to_requeue(
+        self._get_stuck_jobs_to_requeue(
             in_queue_delta=in_queue_delta,
             started_delta=started_delta
-        )
-        for job in jobs:
-            job.requeue()
+        ).requeue()
         return True
 
-    @api.multi
-    def get_stuck_jobs_to_requeue(self, in_queue_delta, started_delta):
-        job_model = self.env['queue.job']
+    @api.model
+    def _get_stuck_jobs_domain(self):
         now = fields.datetime.now()
         queue_dl = now - timedelta(minutes=in_queue_delta)
         started_dl = now - timedelta(minutes=started_delta)
-        stuck_jobs = job_model.search(
-            [('date_enqueued', '<=', fields.Datetime.to_string(queue_dl)),
-             ('state', '=', 'enqueued')]
-        )
-        stuck_jobs += job_model.search(
-            [('date_started', '<=', fields.Datetime.to_string(started_dl)),
-             ('state', '=', 'started')]
-        )
+        return ['|',
+                '&',
+                ('date_enqueued', '<=', fields.Datetime.to_string(queue_dl)),
+                ('state', '=', 'enqueued'),
+                '&',
+                ('date_started', '<=', fields.Datetime.to_string(started_dl)),
+                ('state', '=', 'started')]
+
+    @api.model
+    def _get_stuck_jobs_to_requeue(self, in_queue_delta, started_delta):
+        job_model = self.env['queue.job']
+        stuck_jobs = job_model.search(self._get_stuck_jobs_domain())
         return stuck_jobs
 
 
