4859c54b45ca8f066400e977b66428c7898a7325
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2021-12-03 14:11:42 +0200

    fixup! [MIG][15.0] component_event

--- component_event/tests/test_event.py
+++ component_event/tests/test_event.py
@@ -139,6 +139,7 @@ class TestEvent(ComponentRegistryCase):
 
     def setUp(self):
         super(TestEvent, self).setUp()
+        self._setup_registry(self)
         self._load_module_components("component_event")
 
         # get the collecter to notify the event
